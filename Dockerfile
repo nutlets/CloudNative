97b39cfec972654587508b8076de69596e95980c
Giesen Ian <876865565@qq.com>
2022-07-29 20:02:00 +0800

    java版本应当是1.8而非18；另外添加了dockerfile于目录中。

---
+++ Dockerfile
@@ -0,0 +1,18 @@
+# 基于java镜像创建新镜像
+
+FROM java:8
+
+# 作者
+
+MAINTAINER Giesen
+
+# 将jar包添加到容器中并更名为app.jar
+
+ADD  ./demo-0.0.1-SNAPSHOT.jar ./app/app.jar
+
+# 运行jar包
+
+ENTRYPOINT ["nohup","java","-jar","./app/app.jar","&"]
+
+# docker build -t sbdemo -f ./Dockerfile ./
+# docker run -d -p 8080:8080 --name my_demo_03 sbdemo:latest
--- src/main/java/com/example/demo/Controller.java
+++ src/main/java/com/example/demo/Controller.java
@@ -13,13 +13,13 @@ import java.util.concurrent.TimeUnit;
 public class Controller {
 
 	//设置每秒100次最大访问限制
-	private final RateLimiter limiter = RateLimiter.create(1.0);
+	private final RateLimiter limiter = RateLimiter.create(100.0);
 
 	@RequestMapping("/getMsg")
 	@ResponseBody
 	public Msg getMsg() {
 		//500毫秒内，没拿到令牌，就直接进入服务降级
-		boolean tryAcquire = limiter.tryAcquire(500, TimeUnit.MILLISECONDS);
+		boolean tryAcquire = limiter.tryAcquire(1000, TimeUnit.MILLISECONDS);
 		Msg msg = new Msg();
 
 		if (!tryAcquire)  msg.setMsg("当前访问数过多，获取令牌失败!");
